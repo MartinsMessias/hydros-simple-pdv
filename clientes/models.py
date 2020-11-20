542a81b01c719d9005e540b613cfe3694162af81
MartinsMessias <messiasads2017@gmail.com>
2020-11-20 18:49:57 -0500

    Criação da primeira versão da model de cliente

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=250, blank=False, null=False)
+    cpf_cnpj = models.CharField(max_length=18, unique=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.cpf_cnpj}'
