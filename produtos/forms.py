c6c191a7a05c6d66ce1adb2bba572e41cef0813c
MartinsMessias <messiasads2017@gmail.com>
2020-11-21 12:01:32 -0500

    Adição do form de Produto

---
+++ produtos/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from produtos.models import Produto
+
+
+class ClienteForm(forms.ModelForm):
+
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        # Coloca em todos os campos que não tem uma 'class' do CSS, um 'form-control'.
+        for l in self.base_fields:
+            if not self.base_fields[l].widget.attrs.get('class'):
+                self.base_fields[l].widget.attrs['class'] = 'form-control'
+
+        super(ClienteForm, self).__init__(*args, **kwargs)
