25b51b7ffb711378bbddc83e801fc5e936902ec4
MartinsMessias <messiasads2017@gmail.com>
2020-11-20 19:07:16 -0500

    Criação da primeira versão da model de produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    produto = models.CharField(max_length=256)
+    codigo = models.CharField(max_length=13, unique=True)
+    valor = models.DecimalField(max_digits=11, decimal_places=2)
+    descricao = models.TextField(blank=True, null=True)
+    quantidade = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f'{self.produto} - R$ {self.valor}'
