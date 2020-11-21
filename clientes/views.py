700b444b39afc043c94481a7aff010959470d751
MartinsMessias <messiasads2017@gmail.com>
2020-11-20 21:00:44 -0500

    Adicinando view de cadastro de clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
+from clientes.entities import cliente_entity
+from clientes.forms import ClienteForm
 from clientes.models import Cliente
 from clientes.services import cliente_services
 
 
 class Clientes(View):
     model = Cliente
-    template_name = 'clientes/listar_clientes.html'
 
     def get(self, request, *args, **kwargs):
         context = {'clientes': cliente_services.listar_clientes() }
-        return render(request, self.template_name, context)
+        return render(request, 'clientes/listar_clientes.html', context)
 
+
+    def post(self, request, *args, **kwargs):
+        form_cliente = ClienteForm(request.POST)
+
+        if form_cliente.is_valid():
+            nome = form_cliente.cleaned_data['nome']
+            cpf_cnpj = form_cliente.cleaned_data['cpf_cnpj']
+
+            cliente_novo = cliente_entity.Cliente(nome=nome, cpf_cnpj=cpf_cnpj)
+            cliente_services.cadastrar_cliente(cliente_novo)
+            return redirect('cliente_list')
+        else:
+            form_cliente = ClienteForm()
+
+        return render(request, 'clientes/form_cliente.html', {'form_cliente': form_cliente})
