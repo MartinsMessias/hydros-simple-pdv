8a66baf0c35e09f22b929e8e2fe5b6ef34ccc681
MartinsMessias <messiasads2017@gmail.com>
2020-11-20 20:05:05 -0500

    Criação da primeira versão da view de Clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from clientes.models import Cliente
+from clientes.services import cliente_services
+
+
+class Clientes(View):
+    model = Cliente
+    template_name = ''
+
+    def get(self, request, *args, **kwargs):
+        context = {'clientes': cliente_services.listar_clientes() }
+        return render(request, self.template_name, context)
+
