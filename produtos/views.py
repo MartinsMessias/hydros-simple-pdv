57f62fa6df8b60b7692d0c3d1985ddce5e4bb57a
MartinsMessias <messiasads2017@gmail.com>
2020-11-21 12:05:42 -0500

    Adicionadas as views de Produto:app

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from produtos.forms import ProdutoForm
+from produtos.models import Produto
 
-# Create your views here.
+class ListarProdutosView(ListView):
+    model = Produto
+    template_name = 'produtos/listar_clientes.html'
+
+class CadastrarProdutoView(CreateView):
+    model = Produto
+    template_name = 'produtos/form_produto.html'
+    form_class = ProdutoForm
+    success_url = '/produtos/'
+
+class EditarProdutoView(UpdateView):
+    model = Produto
+    form_class = ProdutoForm
+    template_name = 'produtos/form_produto.html'
+    success_url = '/produtos/'
+
+class RemoverProdutoView(DeleteView):
+    model = Produto
+    success_url = '/produtos/'
+
+class DetalhesProdutoView(DetailView):
+    model = Produto
