d90cb4bb9ed8abadc79490c76cca5842983f1d6b
MartinsMessias <messiasads2017@gmail.com>
2020-11-20 19:25:31 -0500

    Criação da primeira versão da model de vendas

--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from clientes.models import Cliente
+from produtos.models import Produto
+
+
+class Venda(models.Model):
+    FORMAS_DE_PAGAMENTO = (
+        ('Dinheiro', 'Dinheiro'),
+        ('Cartão', 'Cartão'),
+    )
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateField(auto_now=True)
+    forma_pagamento = models.CharField(choices=FORMAS_DE_PAGAMENTO, max_length=8)
+    valor_total = models.DecimalField(max_digits=11, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'{self.data} - {self.cliente.nome}'
+
+
+class VendaDetalhes(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField(default=0)
+    valor = models.DecimalField(max_digits=11, decimal_places=2, default=0)
+
+
+    def __str__(self):
+        return f'{self.produto.produto} - {self.produto.valor}'
